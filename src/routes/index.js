6f055e084cbaa7640093d01b10ac410e2b948979
Jake Moening <jaboc83@gmail.com>
2016-01-07 09:31:26 -0500

    roughed-in other pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,4 +9,47 @@ router.get('/', function(req, res, next) {
 	);
 });
 
+/* GET about page */
+router.get('/about', function(req, res, next) {
+	res.render('about', {
+		title: 'MN Rangers Basketball',
+		page: 'about'}
+	);
+});
+
+/* GET schedule page */
+router.get('/schedule', function(req, res, next) {
+	res.render('schedule', {
+		title: 'MN Rangers Basketball',
+		page: 'schedule'}
+	);
+});
+
+/* GET news page */
+router.get('/news', function(req, res, next) {
+	res.render('news', {
+		title: 'MN Rangers Basketball',
+		page: 'news'}
+	);
+});
+
+
+/* GET gallery page */
+router.get('/gallery', function(req, res, next) {
+	res.render('gallery', {
+		title: 'MN Rangers Basketball',
+		page: 'gallery'}
+	);
+});
+
+
+/* GET contact page */
+router.get('/contact', function(req, res, next) {
+	res.render('contact', {
+		title: 'MN Rangers Basketball',
+		page: 'contact'}
+	);
+});
+
+
 module.exports = router;

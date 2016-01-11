7e5cab43b846190f2d9ac3344b0a6e466e0f95c7
Jake Moening <jaboc83@gmail.com>
2016-01-11 07:43:55 -0500

    #5 - added link to posts from homepage in desktop view

--- src/public/javascripts/index.js
+++ src/public/javascripts/index.js
@@ -15,3 +15,21 @@
 	}
 
 }(document.getElementById.bind(document), window));
+
+/* Navigate to posts page logic */
+(function (getElem, global) {
+	var posts = getElem("posts");
+	function goToSchedule() {
+		global.location.href = "/posts";
+	}
+
+	// For all major browsers, except IE 8 and earlier
+	if(posts.addEventListener) {
+		posts.addEventListener("click", goToSchedule);
+	}
+	// For IE 8 and earlier versions
+	else if (posts.attachEvent) {
+		posts.attachEvent("onclick", goToSchedule);
+	}
+
+}(document.getElementById.bind(document), window));

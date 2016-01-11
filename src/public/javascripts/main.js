06847b299c8800c4fc0f7eae5bf5abb949fff3be
Jake Moening <jaboc83@gmail.com>
2016-01-11 08:00:55 -0500

    #14 - Added Google Analytics

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -3,6 +3,17 @@
 	doc.addEventListener("touchstart", function(){}, true);
 }(document));
 
+/* Google Analytics */
+(function() {
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-72193992-1', 'auto');
+	ga('send', 'pageview');
+}());
+
 /* Handle hamburger menu hide/show menu */
 (function (getElem) {
 	var hamburger = getElem("hamburger-menu");

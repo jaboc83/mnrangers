d6c2f4a060a0452a862f39b8ffde4797fa6d0e26
Jake Moening <jaboc83@gmail.com>
2016-04-09 16:35:14 -0400

    #55 - Move staff into JS objects in data dir

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,36 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var staffArray = require('../data/staff.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', {
 		title: 'MN Rangers Basketball',
-		page: 'index'}
-	);
+		page: 'index'
+	});
 });
 
 /* GET about page */
 router.get('/about', function(req, res, next) {
 	res.render('about', {
 		title: 'MN Rangers Basketball',
-		page: 'about'}
-	);
+		page: 'about',
+		staff: staffArray
+	});
 });
 
 /* GET schedule page */
 router.get('/schedule', function(req, res, next) {
 	res.render('schedule', {
 		title: 'MN Rangers Basketball',
-		page: 'schedule'}
-	);
+		page: 'schedule'
+	});
 });
 
 /* GET headlines page */
 router.get('/headlines', function(req, res, next) {
 	res.render('headlines', {
 		title: 'MN Rangers Basketball',
-		page: 'headlines'}
-	);
+		page: 'headlines'
+	});
 });
 
 
@@ -38,8 +40,8 @@ router.get('/headlines', function(req, res, next) {
 router.get('/community', function(req, res, next) {
 	res.render('community', {
 		title: 'MN Rangers Basketball',
-		page: 'community'}
-	);
+		page: 'community'
+	});
 });
 
 
@@ -47,8 +49,8 @@ router.get('/community', function(req, res, next) {
 router.get('/contact', function(req, res, next) {
 	res.render('contact', {
 		title: 'MN Rangers Basketball',
-		page: 'contact'}
-	);
+		page: 'contact'
+	});
 });
 
 

f9bfadf5bb7df13c161c5e7eba8ad7407cb14cb5
Jake Moening <jaboc83@gmail.com>
2016-05-18 16:06:14 -0400

    fix for index page links to articles

--- src/public/javascripts/index.js
+++ src/public/javascripts/index.js
@@ -18,18 +18,22 @@
 
 /* Navigate to headlines page logic */
 (function (getElem, global) {
-	var headlines = getElem("headlines");
-	function goToSchedule() {
-		global.location.href = "/headlines";
+	var headlines = getElem("headlines").getElementsByTagName("article");
+	function goToHeadline(href) {
+		return function () {
+			global.location.href = href;
+		}
 	}
 
-	// For all major browsers, except IE 8 and earlier
-	if(headlines.addEventListener) {
-		headlines.addEventListener("click", goToSchedule);
-	}
-	// For IE 8 and earlier versions
-	else if (headlines.attachEvent) {
-		headlines.attachEvent("onclick", goToSchedule);
+	for (var i=headlines.length - 1; i >= 0; i--) {
+		// For all major browsers, except IE 8 and earlier
+		if(headlines[i].addEventListener) {
+			headlines[i].addEventListener("click", goToHeadline(headlines[i].getElementsByTagName("a")[0].href));
+		}
+		// For IE 8 and earlier versions
+		else if (headlines[i].attachEvent) {
+			headlines[i].attachEvent("onclick", goToHeadline(headlines[i].getElementsByTagName("a")[0].href));
+		}
 	}
 
 }(document.getElementById.bind(document), window));
